6216798dbf32795aaa035e586a7aea60a6829307
petja <p.hentschel@posteo.de>
2024-04-03 22:46:51 +0100

    vue.js initial project

---
+++ e-receipts/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <ScanButton />
+  </div>
+</template>
+
+<script>
+import ScanButton from './components/ScanButton.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ScanButton
+  }
+}
+</script>
---
+++ e-receipts/src/components/ScanButton.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+      <button @click="startScanner">Scan QR Code</button>
+      <div id="reader" style="width: 500px; height: 300px;"></div>
+    </div>
+  </template>
+  
+  <script>
+  import { Html5QrcodeScanner } from "html5-qrcode";
+  
+  export default {
+    methods: {
+      startScanner() {
+        const html5QrcodeScanner = new Html5QrcodeScanner(
+          "reader",
+          { fps: 10, qrbox: 250 },
+          /* verbose= */ false
+        );
+        html5QrcodeScanner.render(
+          (decodedText, decodedResult) => {
+            // Do something with the decoded text
+            console.log(`Decoded text: ${decodedText}`, decodedResult);
+          },
+          (errorMessage) => {
+            console.error(errorMessage);
+          }
+        );
+      }
+    }
+  };
+  </script>
+  
