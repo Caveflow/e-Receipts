2b25e44356a8a857aeda1a4105ad10b2a8061582
petja <p.hentschel@posteo.de>
2024-04-07 21:07:10 +0100

    Add vuetify dependency to package.json

---
+++ e-receipts/firebase_login.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAAHMoZLllc6m8rzgggS9IOXI5bqFcJyGk",
+    authDomain: "e-receipts-fdba3.firebaseapp.com",
+    projectId: "e-receipts-fdba3",
+    storageBucket: "e-receipts-fdba3.appspot.com",
+    messagingSenderId: "80630704096",
+    appId: "1:80630704096:web:87816ce20ef644fff0ed85"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
