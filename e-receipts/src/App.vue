658a818c16139b0d7c0412fa6cd18ee5580659da
petja <p.hentschel@posteo.de>
2024-04-09 18:59:17 +0100

    Refactor Vue.js project structure, add vue-router, and remove unused files

--- e-receipts/src/App.vue
+++ e-receipts/src/App.vue
@@ -6,28 +6,23 @@
       </v-list>
       <v-divider></v-divider>
       <v-list density="compact" nav>
-        <v-list-item @click="toggleScanner" prepend-icon="mdi-qrcode-scan" title="QR Scanner" value=""></v-list-item>
-        <v-list-item prepend-icon="mdi-library" title="Library" value=""></v-list-item>
+        <v-list-item to="/" prepend-icon="mdi-home" title="Home" value=""></v-list-item>
+        <v-list-item to="/qr-scanner" prepend-icon="mdi-qrcode-scan" title="QR Scanner" value=""></v-list-item>
+        <v-list-item to="/library" prepend-icon="mdi-library" title="Library" value=""></v-list-item>
       </v-list>
     </v-navigation-drawer>
 
     <v-app-bar :elevation="5">
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-
       <v-app-bar-title>Application</v-app-bar-title>
-
       <template v-slot:append>
-
         <v-btn icon="mdi-magnify"></v-btn>
-
       </template>
     </v-app-bar>
 
     <v-main>
-      <!-- Conditionally display the QR scanner -->
-      <v-container v-if="showScanner">
-        <QrcodeStream @decode="onDecode" @init="onInit" />
-      </v-container>
+      <!-- Router View -->
+      <router-view></router-view>
     </v-main>
   </v-app>
 </template>
@@ -39,34 +34,12 @@ const drawer = ref(null)
 </script>
 
 <script>
-import { QrcodeStream } from 'vue-qrcode-reader';
-
-export default {
-  components: {
-    QrcodeStream,
-  },
+export default {  
+  name: 'App',
   data() {
     return {
       drawer: null,
-      showScanner: false, // Controls the visibility of the QR scanner
     };
-  },
-  methods: {
-    toggleScanner() {
-      this.showScanner = !this.showScanner;
-    },
-    onDecode(content) {
-      console.log(content);
-      // Handle the decoded content here
-      this.showScanner = false; // Optionally hide the scanner after decoding
-    },
-    onInit(promise) {
-      promise.then(() => {
-        console.log('QR scanner is ready!');
-      }).catch(error => {
-        console.error('QR scanner error:', error);
-      });
-    }
   },
 };
 </script>
--- e-receipts/src/main.js
+++ e-receipts/src/main.js
@@ -10,11 +10,18 @@ import { registerPlugins } from '@/plugins'
 // Components
 import App from './App.vue'
 
+// Router
+import router from './router'
+
 // Composables
 import { createApp } from 'vue'
 
 const app = createApp(App)
 
+// Use the router with the Vue application instance
+app.use(router)
+
 registerPlugins(app)
 
+// Mount the app to the DOM
 app.mount('#app')
